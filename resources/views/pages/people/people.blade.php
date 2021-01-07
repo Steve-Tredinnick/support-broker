8c81f680caab94b90eef7783bbbfbe5c33bca8ab
Steve-Tredinnick <steve.tredinnick@gmail.com>
2021-01-07 08:17:03 +0000

    base blade files for sections

--- resources/views/pages/people/people.blade.php
+++ resources/views/pages/people/people.blade.php
@@ -6,7 +6,7 @@
     <div class="col-lg-12">
         <div class="card">
             <div class="card-header">
-                Homepage
+                People
             </div>
             <div class="card-body">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc cursus ultricies rutrum. Phasellus ac condimentum massa. Pellentesque eget sapien rutrum, imperdiet neque semper, bibendum leo. In eu leo tristique, lobortis ex non, consequat tellus. Vestibulum vel velit et urna gravida gravida. Duis consequat aliquam turpis. Vivamus sodales erat dolor, sed interdum turpis porta quis. Etiam bibendum, enim ac malesuada maximus, nulla erat pharetra tortor, et tristique quam purus eu leo. Proin fermentum, nibh eget pellentesque maximus, dui libero ullamcorper lacus, vel dignissim purus neque sed turpis. Proin non commodo metus. Fusce tincidunt blandit erat, sed pretium augue faucibus id. Integer eget dolor vel tortor porttitor porta. Pellentesque facilisis volutpat mi, a pellentesque nisi gravida porta. Donec a egestas augue. Donec in purus non metus consequat malesuada.
