42ae48049923e95ad47734c9011a7da85d0c59cb
Steve-Tredinnick <steve.tredinnick@gmail.com>
2021-01-12 20:00:43 +0000

    fix localisation issue with demo data

--- database/factories/PersonFactory.php
+++ database/factories/PersonFactory.php
@@ -21,8 +21,19 @@ class PersonFactory extends Factory
      */
     public function definition()
     {
+        $this->faker->locale('en-GB');
+
+        $gender = $this->faker->randomElement(['male', 'female']);
+
         return [
-            //
+            'first_name' => $this->faker->firstname($gender),
+            'last_name' => $this->faker->lastName,
+            'email' => $this->faker->safeEmail,
+            'phone' => $this->faker->phoneNumber,
+            'gender' => $gender,
+            'address' => $this->faker->address,
+            'dob' => $this->faker->date($format = 'Y-m-d', $max = 'now'),
+            'password' => bcrypt('secret')
         ];
     }
 }
